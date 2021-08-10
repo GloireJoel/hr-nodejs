019d671e509d5531f6de337eff0e99cafbec7e03
Yann Mulonda <yann.mulonda@icfnext.com>
2021-08-10 13:59:40 -0500

    modified terraform script

--- terraformApply.sh
+++ terraformApply.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Create the node-demo-k8s Kubernetes cluster and 
+# nodeports firewall in Google Cloud where the app will be deployed
 terraform apply -auto-approve 
--- terraformDestroy.sh
+++ terraformDestroy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# Destroy - remove the node-demo-k8s Kubernetes cluster and 
+# nodeports firewall in Google Cloud where the app will be deployed
 terraform destroy -auto-approve 
